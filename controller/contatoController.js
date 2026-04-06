22e861ba100e1ffbb51d8a1676236ba1a409e480
GABRIEL VIEIRA DE SOUSA <gabriel_v_de-sousa@estudante.sesisenai.org.br>
2026-04-06 15:46:38 -0300

    06/04/2026

--- controller/contatoController.js
+++ controller/contatoController.js
@@ -1,29 +1,32 @@
 import * as module from '../model/contatosModel.js';
+import bcrypt from 'bcrypt'
 
-export async function listar(req, res){
+export async function listar(req, res) {
     const todaLista = await module.listarContatos();
     console.log(todaLista)
     res.status(200).json(todaLista);
 }
 
-export async function buscarPorID (req, res){
+export async function buscarPorID(req, res) {
     const resultadoContato = await module.buscarContatoID(id);
     // Tratamento do resultado
-    if (!resultadoContato){
-        res.satus(404).json({ erro: "Usuairo não existe!"})
-    }else{
+    if (!resultadoContato) {
+        res.satus(404).json({ erro: "Usuairo não existe!" })
+    } else {
         res.status(200).json(resultadoContato)
     }
 }
 
-export function criarContato(req, res){
+export async function criarContato(req, res) {
     //id, nome, telefone e email.
-    const {nome, telefone, email} = req.body;
+    const { nome, telefone, email, senha } = req.body;
 
-    if (!nome || !telefone || !email){
-        res.status(422).json({ mensagem: "Dados incompletos!"})
-    }else{
-        const novoContato = module.cadastrarContato(nome,telefone,email);
+    if (!nome || !telefone || !email || !senha) {
+        res.status(422).json({ mensagem: "Dados incompletos!" })
+    } else {
+        const senha_hash = await bcrypt.hash(senha,10)
+
+        const novoContato = module.cadastrarContato(nome, telefone, email, senha_hash);
         res.status(201).json(novoContato);
     }
 }
--- model/contatosModel.js
+++ model/contatosModel.js
@@ -1,50 +1,39 @@
 // Simular um banco de dados
-// let contatos = [
-//   { id: 1, nome: "Fulano", telefone: "04899999-8888", email: "fulano.silva@gmail.com" },
-//   { id: 2, nome: "Ciclano Souza", telefone: "04898888-7777", email: "ciclano.s@uol.com.br" },
-//   { id: 3, nome: "Beltrano Oliveira", telefone: "01197777-6666", email: "beltrano.oli@hotmail.com" },
-//   { id: 4, nome: "Ana Costa", telefone: "02196666-5555", email: "ana.costa@outlook.com" },
-//   { id: 5, nome: "Bruno Ramos", telefone: "03195555-4444", email: "bruno.ramos@gmail.com" },
-//   { id: 6, nome: "Carla Dias", telefone: "05194444-3333", email: "carla.dias@yahoo.com" },
-//   { id: 7, nome: "Diego Lima", telefone: "06193333-2222", email: "diego.lima@gmail.com" },
-//   { id: 8, nome: "Elena Martins", telefone: "07192222-1111", email: "elena.m@empresa.com" },
-//   { id: 9, nome: "Fabio Santos", telefone: "08191111-0000", email: "fabio.santos@gmail.com" },
-//   { id: 10, nome: "Gisele Rocha", telefone: "08590000-9999", email: "gisele.rocha@uol.com" },
-//   { id: 11, nome: "Hugo Mendes", telefone: "09198888-1234", email: "hugo.mendes@gmail.com" }
-// ];
-
 import pool from "../database/db.js";
 
 export async function listarContatos() {
-    try {
-        const resultadoGeral = await pool.query('SELECT * FROM contatos')
-        return resultadoGeral
-    } catch (A) {
-        console.log("Erro ", A)
-        return A
-    }
+  try {
+    const resultListagemGeral = await pool.query("SELECT * FROM Contatos");
+    return resultListagemGeral.rows;
+  } catch (error) {
+    console.log("Erro ao consultar no banco de dados: ", error);
+    return error;
+  }
 }
 
 export async function buscarContatoID(id) {
-    try {
-        const resultadoGeral = await pool.query(
-            'SELECT * FROM contatos WHERE id = $1',
-            [id]
-        )
-        return resultadoGeral
-    } catch (A) {
-        console.log("Erro ", A)
-        return A
-    }
+  try {
+    const resultadoPorID = await pool.query(
+      `SELECT * FROM Contatos WHERE id = ${id}`,
+    );
+    return resultadoPorID.rows;
+  } catch (error) {
+    console.log("Erro ao consultar no banco de dados: ", error);
+    return error;
+  }
 }
 
-export function cadastrarContato(nome, telefone, email) {
-    const novoContato = {
-        id: contatos.length + 1,
-        nome: nome,
-        telefone: telefone,
-        email: email
-    }
-    contatos.push(novoContato);
-    return novoContato;
+export async function cadastrarContato(nome, telefone, email, senha_hash) {
+  try {
+    const inserirContatoDb = await pool.query(`
+            INSERT INTO Contatos (nome, telefone, email, senha_hash)
+            VALUES ('${nome}', '${telefone}', '${email}', '${senha_hash}) 
+            RETURNING *
+        `);
+
+    return inserirContatoDb.rows[0];
+  } catch (error) {
+    console.log("Erro ao consultar no banco de dados: ", error);
+    return error;
+  }
 }
